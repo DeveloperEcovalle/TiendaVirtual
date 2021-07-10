8b5f369d4fe36a4b3f8847714b2cafe72b93da86
root <ccubas@unitru.edu.pe>
2021-07-10 20:09:39 +0000

    update mapa

--- public/js/website/socios.js
+++ public/js/website/socios.js
@@ -235,6 +235,7 @@ let vueSocios = new Vue({
                 }
            });
            this.abrirmarcador(marker);
+           this.vermarcador();
         }
         ,
         abrirmarcador: function(valor){
