be9a1cc8f2a42ec33aa2d6de999f6f08020a73ee
CarlosCubas16 <ccubas@unitru.edu.pe>
2021-08-13 09:57:22 -0500

    update final

--- resources/views/website/micuenta/panel_desk.blade.php
+++ resources/views/website/micuenta/panel_desk.blade.php
@@ -1,5 +1,3 @@
 <div class="col-12 p-4 py-2" style="background-color: #6BBD99;">
-    <p>Hola <b>{{session('cliente')->persona->nombres}}</b> (¿no eres <b>{{session('cliente')->persona->nombres}}</b>? <a href="#" v-on:click.prevent="ajaxSalir()">Cerrar sesión</a>)
-
-        Desde el panel de control de tu cuenta puedes ver tus pedidos recientes, gestionar tus datos de envío y facturación, editar tu contraseña y los detalles de tu cuenta.</p>
+    <p>Hola <b>{{session('cliente')->persona->nombres}}</b> si no fuiste t&uacute; quien inicio sesi&oacute;n dale click en <a href="#" v-on:click.prevent="ajaxSalir()">Cerrar sesión</a>. Recuerda, que desde el panel control de tu cuenta puedes ver tus pedidos, gestionar tus datos de env&iacute;o, facturaci&oacute;n, editar tu contraseña y los detalles de tu cuenta.</p>
 </div>
