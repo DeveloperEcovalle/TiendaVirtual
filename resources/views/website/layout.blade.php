124f5a31c51ade9f2b20a22be623a83c5844b4a2
CarlosCubas16 <ccubas@unitru.edu.pe>
2021-09-01 15:22:23 -0500

    update verification

--- resources/views/website/layout.blade.php
+++ resources/views/website/layout.blade.php
@@ -15,6 +15,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="google-site-verification" content="eHWxvf7TsFxAYJGROgzLMMS6zX1VI-iYYcRuCi2_Mu8" />
 
     <title>Ecovalle | @yield('title')</title>
     <link rel="icon" href="{{ asset('img/ecologo.ico') }}" />
